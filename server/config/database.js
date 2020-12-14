5519bc37e03f7b5b318702e6dda505dfdc670ef8
George Manikas <georman@outlook.com.gr>
2020-12-14 20:30:10 +0200

    secured mongo credentials

--- server/config/database.js
+++ server/config/database.js
@@ -4,16 +4,16 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'mongoose',
       settings: {
-        host: env('DATABASE_HOST', 'portfolio.rbiwu.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'server'),
-        username: env('DATABASE_USERNAME', 'georman'),
-        password: env('DATABASE_PASSWORD', 'Xanthi70979!1979'),
+        host: env('DATABASE_HOST'),
+        srv: env.bool('DATABASE_SRV'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
       options: {
         authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        ssl: env.bool('DATABASE_SSL'),
       },
     },
   },
