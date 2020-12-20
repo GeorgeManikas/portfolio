737daded9b1882c6fd9453a4880cd4bb2b7c3d4b
GeorgeManikas <georman@outlook.com.gr>
2020-12-20 08:01:15 +0200

    Site seo

--- client/next-seo.config.js
+++ client/next-seo.config.js
@@ -2,7 +2,7 @@ export default {
     openGraph: {
       type: 'website',
       locale: 'en_IE',
-      url: 'https://www.url.ie/',
+      url: 'https://portfolio.georgemanikas.vercel.app/',
       site_name: 'George Manikas Portfolio Site',
       description:'Front end enthusiast'
     },
